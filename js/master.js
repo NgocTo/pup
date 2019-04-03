ea06d43355581b6dcee9f798188cd1dc277606f6
NgocTo <baongoc1308@gmail.com>
2019-04-03 15:49:43 -0400

    combine and fix sayings

--- js/master.js
+++ js/master.js
@@ -1,27 +1,32 @@
 $(document).ready(function(){
     $("#intro > a").click(function(e){
         e.preventDefault();
-        $("#intro").fadeOut(1500);
-        $("#overlay").fadeOut(1500);
+        $("#intro").fadeOut(700);
+        $("#overlay").fadeOut(700);
         displayInterface();
     });
 
     function displayInterface() {
-        $("#overlay").removeClass('hide');
+        $("#controls").css("width", "100%");
     }
     $("#black").click(function() {
         blackDog();
     })
-
     $("#white").click(function() {
         whiteDog();
     })
-
     $("#brown").click(function() {
         brownDog();
     })
-
     $("#gold").click(function() {
         goldDog();
     })
+    $(".color").on("click", function () {
+        $(".color").removeClass("selected");
+        $(this).addClass("selected");
+    });
+    $(".mood").on("click", function () {
+        $(".mood").removeClass("selected");
+        $(this).addClass("selected");
+    });
 });
